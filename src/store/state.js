905a382f6499f1d06daad09c0707d91efc1460b0
ustbhuangyi <280309453@qq.com>
2017-05-11 00:03:20 +0800

    优化

--- src/store/state.js
+++ src/store/state.js
@@ -2,12 +2,12 @@ import {playMode} from 'common/js/config'
 
 const state = {
   singer: {},
+  playing: false,
   fullScreen: false,
   playlist: [],
   sequenceList: [],
-  currentIndex: -1,
   mode: playMode.sequence,
-  playing: false
+  currentIndex: -1
 }
 
 export default state
