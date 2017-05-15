252949e51cc8faba15e7fa5b36a47654a2bed1ff
ustbhuangyi <280309453@qq.com>
2017-05-16 00:36:21 +0800

    优化

--- src/api/rank.js
+++ src/api/rank.js
@@ -5,6 +5,7 @@ export function getTopList() {
   const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg'
 
   const data = Object.assign({}, commonParams, {
+    uin: 0,
     needNewCode: 1,
     platform: 'h5'
   })
