3cc65a7a28fe27a3a8fd7b306fdd94e209711e61
ustbhuangyi <280309453@qq.com>
2017-05-21 17:04:07 +0800

    优化

--- src/components/playlist/playlist.vue
+++ src/components/playlist/playlist.vue
@@ -6,7 +6,7 @@
           <h1 class="title">
             <i class="icon" :class="iconMode" @click="changeMode"></i>
             <span class="text">{{title}}</span>
-            <span class="clear" @click="clear"><i class="icon-clear"></i></span>
+            <span class="clear" @click="showConfirm"><i class="icon-clear"></i></span>
           </h1>
         </div>
         <scroll ref="listContent" :data="sequenceList" class="list-content">
@@ -71,7 +71,7 @@
       hide() {
         this.showFlag = false
       },
-      clear() {
+      showConfirm() {
         this.$refs.confirm.show()
       },
       confirmClear() {
