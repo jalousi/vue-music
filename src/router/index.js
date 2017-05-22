233adecbf20fb39e0040c7bbd037036906549784
ustbhuangyi <280309453@qq.com>
2017-05-22 18:58:37 +0800

    编译打包

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,55 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Recommend from 'components/recommend/recommend'
-import Singer from 'components/singer/singer'
-import Rank from 'components/rank/rank'
-import Search from 'components/search/search'
-import SingerDetail from 'components/singer-detail/singer-detail'
-import Disc from 'components/disc/disc'
-import TopList from 'components/top-list/top-list'
-import UserCenter from 'components/user-center/user-center'
 
 Vue.use(Router)
+
+const Recommend = (resolve) => {
+  import('components/recommend/recommend').then((recommend) => {
+    resolve(recommend)
+  })
+}
+
+const Singer = (resolve) => {
+  import('components/singer/singer').then((singer) => {
+    resolve(singer)
+  })
+}
+
+const SingerDetail = (resolve) => {
+  import('components/singer-detail/singer-detail').then((singerDetail) => {
+    resolve(singerDetail)
+  })
+}
+
+const Disc = (resolve) => {
+  import('components/disc/disc').then((disc) => {
+    resolve(disc)
+  })
+}
+
+const Rank = (resolve) => {
+  import('components/rank/rank').then((rank) => {
+    resolve(rank)
+  })
+}
+
+const Search = (resolve) => {
+  import('components/search/search').then((search) => {
+    resolve(search)
+  })
+}
+
+const UserCenter = (resolve) => {
+  import('components/user-center/user-center').then((userCenter) => {
+    resolve(userCenter)
+  })
+}
+
+const TopList = (resolve) => {
+  import('components/top-list/top-list').then((topList) => {
+    resolve(topList)
+  })
+}
 
 export default new Router({
   routes: [
