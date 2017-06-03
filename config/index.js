a68e860ce32c90b1a6bbced3fe821793dd635714
ustbhuangyi <280309453@qq.com>
2017-06-03 22:45:49 +0800

    fix config

--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 var path = require('path')
-// 开发环境配置
 module.exports = {
   build: {
     env: require('./prod.env'),
@@ -28,6 +27,7 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
