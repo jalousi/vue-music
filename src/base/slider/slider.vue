8fddeb89aed32f0f4dd6d01482914bf26068e09f
ustbhuangyi <280309453@qq.com>
2017-06-06 12:15:01 +0800

    fix issue#7

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -103,8 +103,13 @@
           this.currentPageIndex = pageIndex
 
           if (this.autoPlay) {
-            clearTimeout(this.timer)
             this._play()
+          }
+        })
+
+        this.slider.on('beforeScrollStart', () => {
+          if (this.autoPlay) {
+            clearTimeout(this.timer)
           }
         })
       },
