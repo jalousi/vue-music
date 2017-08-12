b1a3d44bfd0b8dcec0107a58602140597ec8500e
taohong000 <th19891014@qq.com>
2017-08-12 17:32:22 +0800

    简化路由懒加载模块导入代码

--- src/router/index.js
+++ src/router/index.js
@@ -3,53 +3,14 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-const Recommend = (resolve) => {
-  import('components/recommend/recommend').then((module) => {
-    resolve(module)
-  })
-}
-
-const Singer = (resolve) => {
-  import('components/singer/singer').then((module) => {
-    resolve(module)
-  })
-}
-
-const Rank = (resolve) => {
-  import('components/rank/rank').then((module) => {
-    resolve(module)
-  })
-}
-
-const Search = (resolve) => {
-  import('components/search/search').then((module) => {
-    resolve(module)
-  })
-}
-
-const SingerDetail = (resolve) => {
-  import('components/singer-detail/singer-detail').then((module) => {
-    resolve(module)
-  })
-}
-
-const Disc = (resolve) => {
-  import('components/disc/disc').then((module) => {
-    resolve(module)
-  })
-}
-
-const TopList = (resolve) => {
-  import('components/top-list/top-list').then((module) => {
-    resolve(module)
-  })
-}
-
-const UserCenter = (resolve) => {
-  import('components/user-center/user-center').then((module) => {
-    resolve(module)
-  })
-}
+const Recommend = () => import('components/recommend/recommend')
+const Singer = () => import('components/singer/singer')
+const Rank = () => import('components/rank/rank')
+const Search = () => import('components/search/search')
+const SingerDetail = () => import('components/singer-detail/singer-detail')
+const Disc = () => import('components/disc/disc')
+const TopList = () => import('components/top-list/top-list')
+const UserCenter = () => import('components/user-center/user-center')
 
 export default new Router({
   routes: [
