86e6ba950114cd1f5f85a7b3e5661151b7721f16
ustbhuangyi <280309453@qq.com>
2017-08-19 01:57:27 +0800

    fix issue #75

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -120,8 +120,9 @@
         } else if (index > this.listHeight.length - 2) {
           index = this.listHeight.length - 2
         }
-        this.scrollY = -this.listHeight[index]
+//        this.scrollY = -this.listHeight[index]
         this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
+        this.scrollY = this.$refs.listview.scroll.y
       }
     },
     watch: {
