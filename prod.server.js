4193f22f33c5d299f88b3da3a1ef1b60d69aae07
ustbhuangyi <280309453@qq.com>
2017-10-08 15:31:46 +0800

    for product env

--- prod.server.js
+++ prod.server.js
@@ -24,6 +24,30 @@ apiRoutes.get('/getDiscList', function (req, res) {
   })
 })
 
+apiRoutes.get('/getCdInfo', function (req, res) {
+  var url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    var ret = response.data
+    if (typeof ret === 'string') {
+      var reg = /^\w+\(({.+})\)$/
+      var matches = ret.match(reg)
+      if (matches) {
+        ret = JSON.parse(matches[1])
+      }
+    }
+    res.json(ret)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+
 apiRoutes.get('/lyric', function (req, res) {
   var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
 
@@ -36,7 +60,7 @@ apiRoutes.get('/lyric', function (req, res) {
   }).then((response) => {
     var ret = response.data
     if (typeof ret === 'string') {
-      var reg = /^\w+\(({[^\(\)]+})\)$/
+      var reg = /^\w+\(({.+})\)$/
       var matches = response.data.match(reg)
       if (matches) {
         ret = JSON.parse(matches[1])
