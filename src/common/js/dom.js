05ba2aa9f96eb83c708f5f76a5fd025e00d3e6f0
xinx1n <xin9142@qq.com>
2018-01-09 09:49:44 +0800

    改进 hasClass 和 addClass 方法

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -1,16 +1,9 @@
 export function hasClass(el, className) {
-  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
-  return reg.test(el.className)
+  return el.classList.contains(className)
 }
 
 export function addClass(el, className) {
-  if (hasClass(el, className)) {
-    return
-  }
-
-  let newClass = el.className.split(' ')
-  newClass.push(className)
-  el.className = newClass.join(' ')
+  el.classList.add(className)
 }
 
 export function getData(el, name, val) {
