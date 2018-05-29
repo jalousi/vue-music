f475b4e486ad3a09a189900cb230fcdaaf9f1cae
huangbin <18358274764@163.com>
2018-05-29 13:35:46 +0800

    eslint

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -1,9 +1,9 @@
-export function hasClass(el,className){
+export function hasClass(el, className) {
   let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
   return reg.test(el.className)
 }
-export function addClass(el,className){
-  if(hasClass(el,className)){
+export function addClass(el, className) {
+  if (hasClass(el, className)) {
     return
   }
   let newClass = el.className.split(' ')
