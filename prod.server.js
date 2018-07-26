be096f36922598aa877109c82f238471131254d0
ustbhuangyi <280309453@qq.com>
2018-07-26 13:55:43 +0800

    fix issue #136

--- prod.server.js
+++ prod.server.js
@@ -48,7 +48,6 @@ apiRoutes.get('/getCdInfo', function (req, res) {
   })
 })
 
-
 apiRoutes.get('/lyric', function (req, res) {
   var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
 
@@ -81,6 +80,21 @@ app.post('/api/getPurlUrl', bodyParser.json(), function (req, res) {
       origin: 'https://y.qq.com',
       'Content-type': 'application/x-www-form-urlencoded'
     }
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+apiRoutes.get('/search', function (req, res) {
+  const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
   }).then((response) => {
     res.json(response.data)
   }).catch((e) => {
--- src/api/search.js
+++ src/api/search.js
@@ -1,5 +1,8 @@
 import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
+import axios from 'axios'
+
+const debug = process.env.NODE_ENV !== 'production'
 
 export function getHotKey() {
   const url = 'https://c.y.qq.com/splcloud/fcgi-bin/gethotkey.fcg'
@@ -14,7 +17,7 @@ export function getHotKey() {
 }
 
 export function search(query, page, zhida, perpage) {
-  const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
+  const url = debug ? '/api/search' : 'http://ustbhuangyi.com/music/api/search'
 
   const data = Object.assign({}, commonParams, {
     w: query,
@@ -31,8 +34,13 @@ export function search(query, page, zhida, perpage) {
     remoteplace: 'txt.mqq.all',
     uin: 0,
     needNewCode: 1,
-    platform: 'h5'
+    platform: 'h5',
+    format: 'json'
   })
 
-  return jsonp(url, data, options)
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
 }
